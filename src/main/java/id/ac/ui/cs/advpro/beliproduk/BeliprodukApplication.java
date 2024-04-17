f9cbbc4e54e4fcc7ce401b272ede862951f3ba5b
aldenluthfi <alden.luthfi@ui.ac.id>
2024-04-17 18:30:45 +0700

    First Commit!

---
+++ src/main/java/id/ac/ui/cs/advpro/beliproduk/BeliprodukApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advpro.beliproduk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeliprodukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeliprodukApplication.class, args);
+	}
+
+}
