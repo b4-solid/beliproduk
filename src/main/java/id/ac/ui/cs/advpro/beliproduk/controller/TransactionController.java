eccdea99ac5c9dc6e66c98242a6fc0bb7a02133c
aldenluthfi <aldenluthfi@gmail.com>
2024-05-08 20:39:17 +0700

    Create Controller

--- src/main/java/id/ac/ui/cs/advpro/beliproduk/controller/TransactionController.java
+++ src/main/java/id/ac/ui/cs/advpro/beliproduk/controller/TransactionController.java
@@ -1,5 +1,25 @@
 package id.ac.ui.cs.advpro.beliproduk.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import id.ac.ui.cs.advpro.beliproduk.models.TransactionModel;
+import id.ac.ui.cs.advpro.beliproduk.service.TransactionService;
+
+@RestController
+@RequestMapping("/api")
 public class TransactionController {
-    
+
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public TransactionModel addTransaction(@RequestBody TransactionModel transaction) {
+        return transactionService.addTransaction(transaction);
+    }
 }
